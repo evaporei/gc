975c35c754b1188477a32b1f20f3d8b6feaec40b
Eva Pace <eba.pachi@gmail.com>
2023-11-13 16:41:30 -0300

    lil vm

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub struct GcPtr<T>(NonNull<T>);
 
 pub struct Object {
     marked: bool,
-    typ: ObjType,
+    value: ObjType,
 }
 
 pub enum ObjType {
@@ -15,4 +15,47 @@ pub enum ObjType {
 pub struct Pair {
     head: Option<GcPtr<Object>>,
     tail: Option<GcPtr<Object>>,
+}
+
+const STACK_MAX: usize = 256;
+
+pub struct Vm {
+    stack: [Option<GcPtr<Object>>; STACK_MAX],
+    stack_size: usize,
+}
+
+impl Vm {
+    pub fn new() -> Self {
+        Self {
+            stack: std::array::from_fn(|_| None),
+            stack_size: 0,
+        }
+    }
+
+    pub fn push(&mut self, value: ObjType) {
+        assert!(self.stack_size < STACK_MAX, "Stack overflow!");
+        let mut box_obj = Box::new(Object {
+            marked: false,
+            value,
+        });
+        self.stack[self.stack_size] = Some(GcPtr(NonNull::new(&mut *box_obj).unwrap()));
+        std::mem::forget(box_obj);
+        self.stack_size += 1;
+    }
+
+    pub fn pop(&mut self) -> GcPtr<Object> {
+        let obj = self.stack[self.stack_size].take().unwrap();
+        self.stack_size -= 1;
+        obj
+    }
+
+    pub fn push_int(&mut self, value: i64) {
+        self.push(ObjType::Int(value));
+    }
+
+    pub fn push_pair(&mut self) {
+        let head = Some(self.pop());
+        let tail = Some(self.pop());
+        self.push(ObjType::Pair(Pair { head, tail }));
+    }
 }
